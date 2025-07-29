88d08d51a0808b25fe4552a0e8aa4cb4c7f22898
rvan-duy <rvanduyneveldt@gmail.com>
2025-07-29 15:40:32 +0200

    Add MongoDB connection setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 
+import connectDB from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
 
 dotenv.config();
@@ -8,6 +9,8 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 
